025bd3cd7989f78b1fa64decb60cf844245b07d2
Max Erenberg <merenber@csclub.uwaterloo.ca>
2024-08-17 15:39:17 -0400

    add test to delete TOTP account

---
+++ src/components/AccountSettings.test.ts
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/svelte";
+import userEvent, { type UserEvent } from "@testing-library/user-event";
+import { afterEach, beforeEach, expect, test } from "vitest";
+
+import App from "../App.svelte";
+import {
+  addTOTPAccount,
+  commonBeforeEach,
+  commonAfterEach,
+  encodedUnencryptedSettings,
+  saveEncodedSettings,
+} from "../test/utils";
+
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+
+async function deleteTOTPAccount(user: UserEvent, issuer: string) {
+  await user.click(await screen.findByText(issuer));
+  await user.click(
+    await screen.findByRole("button", { name: "Account settings" }),
+  );
+  await user.click(
+    await screen.findByRole("button", { name: "Remove account" }),
+  );
+  await user.click(await screen.findByRole("button", { name: "Continue" }));
+}
+
+test("Delete TOTP account", async () => {
+  const user = userEvent.setup();
+  saveEncodedSettings(encodedUnencryptedSettings);
+  render(App);
+  await addTOTPAccount(user, {
+    issuer: "Gmail",
+    name: "jdoe@gmail.com",
+  });
+  await addTOTPAccount(user, {
+    issuer: "Outlook",
+    name: "jdoe@live.com",
+    digits: 8,
+  });
+
+  await deleteTOTPAccount(user, "Gmail");
+  await screen.findByText("Outlook");
+  expect(screen.queryByText("Gmail")).not.toBeInTheDocument();
+
+  await deleteTOTPAccount(user, "Outlook");
+  await screen.findByText("You don't have any accounts yet.");
+});
